e25ece8c1544dfb8b77d3f1218b9dcf123667de7
Rildian <130923873+Rildian@users.noreply.github.com>
2024-12-08 17:35:37 -0300

    removing users and setting new passwords for them
    removing users and setting new passwords for them

---
+++ queriesCriadasUdemy/deletingUsersnSettingPasswords.sql
@@ -0,0 +1,9 @@
+-- tem que especificar o dominio dele
+DROP USER BubumGuloso@localhost;
+/* tipo o do alysson que havia sido criado c/ o 
+IP DELE*/ 
+DROP USER alyssonbreno@192.168.1.100;
+
+-- vamos setar senhas agr
+SET PASSWORD FOR Rildian@localhost = '#273927@Aasjdoa';
+-- SET PASSWORD = '444' minha senha seria essa
